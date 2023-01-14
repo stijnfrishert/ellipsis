e0654f678f7095a7419466246f2d648eeac7e991
Stijn Frishert <stijnfrishert@gmail.com>
2023-01-14 22:35:14 +0100

    Better is_valid

--- src/utils.rs
+++ src/utils.rs
@@ -1,5 +1,11 @@
 pub fn sanitize(str: &str) -> String {
-    if str.chars().all(|c| c.is_alphanumeric() || c == '_') {
+    let is_valid = match str.chars().next() {
+        Some(x) if x.is_numeric() => str.parse::<f32>().is_ok(),
+        Some(_) => str.chars().all(|c| c.is_alphanumeric() || c == '_'),
+        None => return String::new(),
+    };
+
+    if is_valid {
         str.to_string()
     } else {
         format!("\"{str}\"")
